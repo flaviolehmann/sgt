d2533381a6c4657a626c755636681beaacacec08
Jean Pandolfi <jean.pandolfi@anvisa.gov.br>
2021-07-27 15:26:24 -0300

    Criação do componentes e requisições

--- src/main/java/br/com/basis/sgt/web/rest/TarefaResource.java
+++ src/main/java/br/com/basis/sgt/web/rest/TarefaResource.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tarefas")
 @RequiredArgsConstructor
+@CrossOrigin()
 public class TarefaResource {
 
     private final TarefaService tarefaService;
