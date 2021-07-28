3b7f62fb9c3b98a0139d7f365274451479a66930
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-07-27 22:16:20 -0300

    Atualizacao - 27-07-2021/22:16

--- src/main/java/br/com/basis/sgt/domain/Responsavel.java
+++ src/main/java/br/com/basis/sgt/domain/Responsavel.java
@@ -4,9 +4,15 @@ package br.com.basis.sgt.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+;import java.util.List;
 
 @Entity
 @Table(name = "responsavel")
--- src/main/java/br/com/basis/sgt/service/dto/ResponsavelDTO.java
+++ src/main/java/br/com/basis/sgt/service/dto/ResponsavelDTO.java
@@ -1,10 +1,16 @@
 package br.com.basis.sgt.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class ResponsavelDTO {
 
     private Long id;
